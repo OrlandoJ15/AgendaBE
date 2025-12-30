4d16d3a3ba033cfb5187d24bedec2dddb6a6798e
Orlando J. Rojas Gonzalez <67527098+OrlandoJ15@users.noreply.github.com>
2025-12-30 16:50:58 -0600

    5
    5

---
+++ DataAccess/Interfaces/IPacienteDA.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Interfaces
+{
+    public interface IPacienteDA
+    {
+        public List<Expediente> GetAllRecords();
+
+        List<Expediente> GetRecordByName(string primerNom, string segundoNom, string primerAp, string segundoAp);
+
+        List<Expediente> GetRecordByIdentification(string pidentificacion, string pcod_tipdoc);
+
+        List<string> InsertNewRecord(Expediente pExpediente);
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# AgendaBE
-# AgendaBE
